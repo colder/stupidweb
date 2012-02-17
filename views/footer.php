27e3a05b843f824091850b73eded02e76aba33aa
Etienne Kneuss <colder@php.net>
2012-02-17 04:12:36 +0100

    Fix padding of content to make it more readable, implement "last update using git log"

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,12 @@
         </div>
         <!-- FOOTER -->
-        <div id="footer"><span>Last update: ...</span></div>
+        <div id="footer"><span>Last update: 
+<?php
+    $root = __ROOT__;
+
+    echo `cd $root; git log --pretty=format:'%ar by %an' | head -n1`
+?>
+</span></div>
     </body>
 </html>
 
