d200b6a83e49037faf26f65b7d6409ac92e95518
Etienne Kneuss <colder@php.net>
2012-02-22 16:16:05 +0100

    Add contact info into markdown

--- views/about.php
+++ views/about.php
@@ -4,16 +4,9 @@
        <img id="profile" src="/images/profile.jpg" />
 
        <h2>Contact</h2>
-       <h3>E-Mail</h3>
-       <p>etienne.kneuss@epfl.ch</p>
-       <p>colder@php.net</p>
-       <p>ekneuss@gmail.com</p>
-
-       <h3>IRC</h3>
-       <p>ekneuss<br />(Freenode, QuakeNet, EFnet)</p>
-
-       <h3>Twitter</h3>
-       <p><a href="http://twitter.com/ekneuss" target="_blank">@ekneuss</a></p>
+        <?php
+            display_markdown('about/contact');
+        ?>
       </div>
     </div>
 
